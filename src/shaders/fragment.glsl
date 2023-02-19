65cc0b305a540f83167aa262abf05dae002a1191
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2023-02-19 16:42:01 +0100

    Add circle solution

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -2,6 +2,12 @@ uniform vec2 u_resolution;
 uniform float u_time;
 
 void main() {
+  vec2 pos = gl_FragCoord.xy / min(u_resolution.xy.x, u_resolution.xy.y);
 
-  gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+  if(pow(pos.x - 0.5, 2.0) + pow(pos.y - 0.5, 2.0) < pow(0.4, 2.0)) {
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  } else {
+    gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+  }
+
 }
