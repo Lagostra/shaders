22749d8d42115f7457f99f6c704c6533395b3dfc
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2023-02-19 16:34:05 +0100

    Add square solution

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -2,6 +2,12 @@ uniform vec2 u_resolution;
 uniform float u_time;
 
 void main() {
+  vec2 pos = gl_FragCoord.xy / u_resolution.xy;
 
-  gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+  if(pos.x > 0.2 && pos.x < 0.8 && pos.y > 0.2 && pos.y < 0.8) {
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  } else {
+    gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+  }
+
 }
